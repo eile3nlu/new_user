f86dae8e0cea541bdce16db505fccfb2c87c7bbc
chriskoh <trchriskoh@gmail.com>
2017-03-30 11:05:29 -0700

    generate_creds.sh, stores SecretServer login credentials

--- generate_creds.sh
+++ generate_creds.sh
@@ -1,2 +1,17 @@
 #!/bin/bash 
 # generate credentials
+
+#SecretServer
+ssusername=$1
+sspassword=$2
+
+cat > .credentials.json <<- EOM
+{
+    "secretserver":{
+        "username": "$ssusername",
+        "password": "$sspassword",
+        "orgcode": "JKGD9",
+        "domain": "keypr.com"
+    }
+}
+EOM
