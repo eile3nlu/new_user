ff0b174dc7c651bb3450695ac8b6e70b0db8f831
chriskoh <trchriskoh@gmail.com>
2017-03-31 00:38:32 -0700

    jumpcloud new user automation updated

--- jumpcloud.sh
+++ jumpcloud.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 
+# get user information from .new_user.json
 apikey=$(cat .credentials.json | jq ".jumpcloud.apikey" | sed "s/\"//g")
 fName=$(cat .new_user.json | jq ".fName")
 lName=$(cat .new_user.json | jq ".lName")
 email=$(cat .new_user.json | jq ".email")
 username=$(cat .new_user.json | jq ".username")
 password=$(cat .new_user.json | jq ".password")
+unixid=$(cat .new_user.json | jq ".unixid")
+group=$(cat .new_user.json | jq ".role")
 
+# modify group settings when role is defined
+if [ "$group" == "staff" ]
+then
+    groupid=6001
+else
+    groupid=6001
+fi
 
+# create new user in jumpcloud
 curl \
-    -d '{"firstname": '$fName', "lastname": '$lName', "email" : '$email', "username" : '$username', "password": '$password'}' \
+    -d '{"unix_guid": '$groupid', "unix_uid": '$unixid', "firstname": '$fName', "lastname": '$lName', "email" : '$email', "username" : '$username', "password": '$password'}' \
     -X 'POST' \
     -H 'Content-Type: application/json' \
     -H 'Accespt: application/json' \
     -H 'x-api-key: '$apikey'' \
-    "https://console.jumpcloud.com/api/systemusers"
+    'https://console.jumpcloud.com/api/systemusers'
--- new_user.sh
+++ new_user.sh
@@ -4,7 +4,8 @@ fName=$1
 lName=$2
 emailPersonal=$3
 role=$4
-ticketNum=$5
+unixid=$5
+ticketNum=$6
 password=$(cat /dev/urandom | LC_CTYPE=C tr -dc 'a-zA-Z0-9' | fold -w 12 | head -n 1)
 
 cat > .new_user.json <<- EOM
@@ -17,11 +18,13 @@ cat > .new_user.json <<- EOM
     "emailPersonal": "$emailPersonal",
     "username": "${fName:0:1}$lName",
     "password": "$password",
-    "role": "$role" 
+    "role": "$role",
+    "unixid": "$unixid"
 }
 EOM
 
 python secret_server.py
 python gmail.py
+source jumpcloud.sh
 
 #rm .new_user.json
