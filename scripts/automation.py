a14f207cf67f231ebb8986d60cb2f5db3a5ac538
chriskoh <trchriskoh@gmail.com>
2018-01-31 15:19:28 -0800

    fixed onboarding spreadsheet manipulation

--- scripts/automation.py
+++ scripts/automation.py
@@ -20,6 +20,9 @@ def get_new_users(spreadsheet):
     # open spreadsheets
     onboarding = spreadsheet.get_worksheet(0)
 
+    # count number of lines
+    numLines = onboarding.row_count
+
     # get all data from on-boaridng list 
     listOfUsers = onboarding.get_all_records()
 
@@ -55,28 +58,52 @@ class new_user:
         self.auditRows = self.auditSheet.row_count
         self.auditSheet.insert_row([self.fullName, self.startDate, self.accessDate, self.department, self.role], 2)
 
+        # employee list
+        self.employeeSheet = spreadsheet.get_worksheet(1)
+
+        # onboarding
+        self.onboardingSheet = spreadsheet.get_worksheet(0)
+
     def secret_server(self, action):
 
-        #ss.main(action, self.firstName, self.lastName, self.personalEmail, self.password)
+        ss.main(action, self.firstName, self.lastName, self.personalEmail, self.password)
         self.auditSheet.update_cell(2, 6, 'X')
+
+        if action == 'create':
+            print('---------- Onboarding ----------')
+        else:
+            print('---------- Offboarding ----------')
 
     def gmail(self, action):
 
-        #gmail.main(action, self.firstName, self.lastName, self.fullName, self.keyprEmail, self.personalEmail, self.password, self.role, self.contractor, self.kyiv)
+        gmail.main(action, self.firstName, self.lastName, self.fullName, self.keyprEmail, self.personalEmail, self.password, self.role, self.contractor, self.kyiv)
         self.auditSheet.update_cell(2, 7, 'X')
         self.auditSheet.update_cell(2, 10, 'X')
         self.auditSheet.update_cell(2, 11, 'X')
         self.auditSheet.update_cell(2, 12, 'X')
+        
 
     def jumpcloud(self, action):
         
-        #call(['bash', 'jumpcloud.sh', self.firstName, self.lastName, self.keyprEmail, self.personalEmail, self.userName, self.password, str(self.unixId), self.role, self.contractor.lower(), action])
+        call(['bash', 'jumpcloud.sh', self.firstName, self.lastName, self.keyprEmail, self.personalEmail, self.userName, self.password, str(self.unixId), self.role, self.contractor.lower(), action])
         self.auditSheet.update_cell(2, 8, 'X')
 
     def confluence(self, action): 
 
-        #confluence.main(action, self.fullName, self.password, self.contractor, self.keyprEmail)
+        confluence.main(action, self.fullName, self.password, self.contractor, self.keyprEmail)
         self.auditSheet.update_cell(2, 9, 'X')
+
+    def employee_list_add(self):
+
+        self.employeeSheet.insert_row([self.fullName, self.personalEmail, self.keyprEmail, self.phone, 'Active', self.startDate, self.department, self.role], 2)
+
+    def remove_from_onboarding(self):
+   
+            
+        print('test') 
+        self.onboardingSheet.delete_row(2)
+        self.onboardingSheet.insert_row([], 2)
+        print('removed')
 
 def main():
 
@@ -88,11 +115,15 @@ def main():
 
         onboarding = new_user(user, spreadsheet)
 
+        # onboard new user
         onboarding.secret_server('create')
         onboarding.gmail('create')
         onboarding.jumpcloud('create')
         onboarding.confluence('create')
 
+        # add user to employee list and remove from onboarding sheet
+        onboarding.employee_list_add()
+        onboarding.remove_from_onboarding()
 
 if __name__ == "__main__":
     main()
